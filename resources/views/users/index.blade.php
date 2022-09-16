fb615cedb5ceee72dd134b913d8819e278622e47
thanh <leodomsolar@gmail.com>
2022-09-16 17:38:38 +0700

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layout.layout1')
 @section('title','Quản lý tài khoản')
 @section('main')
-
+    <nav>
+        <button type="button" id="addUserRoleBtn" class="btn btn-primary btn-sm">Thêm loại tài khoản</button>
+    </nav>
 @endsection
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="admin/admin.js"></script>
+
